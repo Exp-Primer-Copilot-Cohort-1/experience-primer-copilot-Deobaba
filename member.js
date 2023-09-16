dff12d50b9f48d3b6b4a70bd79b9f5f0aaba1c5c
Adenekan Adeola <104663055+Deobaba@users.noreply.github.com>
2023-09-16 16:03:57 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,15 +1,9 @@
-function skillMembers() {
-    var members = [];
-    var member = {};
-    member.name = "John";
-    member.age = 30;
-    member.skills = ["Javascript", "HTML", "CSS"];
-    members.push(member);
-    member = {};
-    member.name = "Mary";
-    member.age = 25;
-    member.skills = ["C+", "Java", "Python"];
-    members.push(member);
-    return members;
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/skills-member.html',
+    controller: 'SkillsMemberCtrl',
+    controllerAs: 'skillsMemberCtrl'
+  };
 }
 
